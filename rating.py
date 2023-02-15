ef01531fdc6687392be5bfc05a325a936e20838f
Sukhleen Kaur <74076814+sukhleen-kaur-27@users.noreply.github.com>
2023-02-16 02:20:17 +0530

    Update rating.py

--- rating.py
+++ rating.py
@@ -1,5 +1,5 @@
 import streamlit as st
-@st.cache(suppress_st_warning=True)
+
 def app(qualified_movies_df):
 	st.header("Movies based on Rating using Weighted Rating")
 	qualified_movies_df.sort_values(by= 'weighted_rating', ascending = False, inplace = True)
