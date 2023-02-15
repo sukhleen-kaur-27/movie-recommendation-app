dd2ee296e93a87bebe5a44800b77f6890182ea61
Sukhleen Kaur <74076814+sukhleen-kaur-27@users.noreply.github.com>
2023-02-16 01:29:16 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def recommend_movies(movie_name, similarity_df, all_movies_ratings):
-  similar_movies = similarity_df[str(movie_name).title()]
+  similar_movies = similarity_df[str(movie_name)]
   similar_movies_df = pd.DataFrame(similar_movies)
   similar_movies_df.rename(columns = {similar_movies_df.columns[0]: 'correlation'}, inplace = True)
   corr_num_ratings = similar_movies_df.join(all_movies_ratings['num of ratings'])
