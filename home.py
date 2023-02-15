64897a6adca01dc64ce36ffe9fcbfe29a694782a
Sukhleen Kaur <74076814+sukhleen-kaur-27@users.noreply.github.com>
2023-02-16 01:55:40 +0530

    Update home.py

--- home.py
+++ home.py
@@ -13,14 +13,5 @@ def app(similarity_df,all_movies_ratings):
   st.write('This web app allows a user to get movie recommendations based on popularity, genre and rating, along with some visuals a peek into the actual data.')
   st.text("")
   movie_name = st.text_input("Enter a movie name to get similar recommendations :")
-  hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
-
-  # Inject CSS with Markdown
-  st.markdown(hide_table_row_index, unsafe_allow_html=True)
   if st.button("Search"):
     st.table(recommend_movies(movie_name, similarity_df, all_movies_ratings))
